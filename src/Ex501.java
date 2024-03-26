3063ae05558f649c101f87825e8fbc47ec8d0528
whflwhfl <81403148+whflwhfl@users.noreply.github.com>
2024-03-26 15:48:48 +0900

    ex 5 성공 !!
    good!

---
+++ src/Ex501.java
@@ -0,0 +1,20 @@
+
+public class Ex501 {
+	//Method
+	public static void hello(String name) {
+		System.out.println("Hello, world " + name);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		hello("seungmin");
+		hello("Aaron");
+		hello("Tom");
+		hello("Loki");
+		hello("Rocky");
+		hello("Socky");
+		hello("");
+
+	}
+
+}
