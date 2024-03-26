f270ccc0c5252c5ba5a0fbbd00ded154051fb0ad
whflwhfl <81403148+whflwhfl@users.noreply.github.com>
2024-03-26 16:57:59 +0900

    6 성공!
    good!

---
+++ src/Ex605.java
@@ -0,0 +1,19 @@
+
+public class Ex605 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "Java";
+		String s2 = "Java";
+		String s3 = "java";
+		
+		System.out.println(s1 == s2); //같아요?
+		System.out.println(s1 != s2);
+		System.out.println(s1 == s3);
+		System.out.println(s2 != s3);
+		
+		String s4 = s2.concat(s3);
+		System.out.println(s4);
+	}
+
+}
